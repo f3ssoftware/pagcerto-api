a3a00ac3e795ae28178d13175d177f5396d17a50
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2024-12-24 02:33:12 -0300

    fix: prisma service export

--- src/processor/processor.module.ts
+++ src/processor/processor.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProcessorService } from './processor.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   providers: [ProcessorService],
+  imports: [PrismaModule],
 })
 export class ProcessorModule {}
