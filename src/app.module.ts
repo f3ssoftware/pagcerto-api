144750a3840119c0bd1608b41b106e08c5fbd9c6
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2024-12-24 02:43:26 -0300

    chore: swagger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProcessorModule } from './processor/processor.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [ProcessorModule, PrismaModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
