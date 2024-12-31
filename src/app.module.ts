4c841e75a0cecb31d3bcbc5ee5b26e30fa45e602
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2024-12-30 23:44:24 -0300

    feat: organization module and crud endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { ProcessorModule } from './processor/processor.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ClientModule } from './client/client.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { OrganizationModule } from './organization/organization.module';
 
 @Module({
-  imports: [ProcessorModule, PrismaModule, ClientModule, TransactionModule],
+  imports: [
+    ProcessorModule,
+    PrismaModule,
+    ClientModule,
+    TransactionModule,
+    OrganizationModule,
+  ],
   controllers: [],
   providers: [AppService],
 })
