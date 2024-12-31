c2e9de85fb7c9f58ee2aab95c003db19714f5b73
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2024-12-31 00:03:41 -0300

    chore: updates dtos with swagger and  class validaors

--- src/cost-center/dto/create-cost-center.dto.ts
+++ src/cost-center/dto/create-cost-center.dto.ts
@@ -1,4 +1,16 @@
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCostCenterDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ description: 'Name of the cost center' })
   name: string;
+
+  @IsNotEmpty()
+  @IsUUID()
+  @ApiProperty({
+    description: 'ID of the organization to which the cost center belongs',
+  })
   organizationId: string;
 }
--- src/organization/dto/create-organization.dto.ts
+++ src/organization/dto/create-organization.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateOrganizationDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ description: 'Name of the organization' })
   name: string;
 }
