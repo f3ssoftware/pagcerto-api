fb7d69e9a73fff4fc68260297b813ac28b7c11ba
Felipe Sampaio Marques <f3ssoftware@gmail.com>
2025-01-16 15:04:15 -0300

    feat: crypto payment method

--- src/checkout/dto/pay-checkout.dto.ts
+++ src/checkout/dto/pay-checkout.dto.ts
@@ -1,8 +1,10 @@
 import { PaymentMethod } from '@prisma/client';
+import { Countries, CountriesEnum } from '../../shared/countries.enum';
 
 export class PayCheckoutDto {
   payment_method: PaymentMethod;
   credit_card?: CreditCardPayment;
+  address: BuyerAddress;
 }
 
 class CreditCardPayment {
@@ -11,4 +13,15 @@ class CreditCardPayment {
   holdername: string;
   expiration_month: string;
   expiration_year: string;
+}
+
+class BuyerAddress {
+  city: string;
+  country: CountriesEnum;
+  state: string;
+  address1: string;
+  address2: string;
+  zipcode: string;
+  complement: string;
+  number: string;
 }
